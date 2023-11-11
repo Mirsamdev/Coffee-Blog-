71c8897ee302d0b8c79f30267b94199f68198215
Mirsandev <xmirandadevx@gmail.com>
2023-11-11 15:17:43 -0400

    agregar a html

--- js/scripts.js
+++ js/scripts.js
@@ -8,7 +8,35 @@ console.log(heading);
 
 // querySelectorAll
 const enlaces = document.querySelectorAll('.navegation a');
-console.log(enlaces);
+enlaces[0].textContent = 'Nuevo Texto para Enlace';
+enlaces[0].classList.add('nueva-clase');
+// enlaces[0].classList.remove('navegacion_enlace');
+
+// Generar un nuevo enlace
+const nuevoEnlace = document.createElement('A')
+
+// Agregar el href
+nuevoEnlace.href = 'nuevo-enlace.html';
+// Agregar el texto
+nuevoEnlace.href = 'nuevo-enlace.html';
+// Agregar la clase
+nuevoEnlace.classList.add('navegacion_enlace');
+
+
+// Agregarlo al documento
+const navegation = document.querySelector('.navegation');
+navegation.appendChild(nuevoEnlace)
+
+
+
+
+
+// Event Listener
+
+
+console.log(nuevoEnlace);
+
+
 
 
 
