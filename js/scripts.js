011e5eb02e2df1757b181664a077f07723b297ce
Mirsandev <xmirandadevx@gmail.com>
2023-11-11 14:40:14 -0400

    QuerySelectors

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+// querySelector
+const heading = document.querySelector(".header__text h2"); // retorna 0 o 1 elemntos
+heading.textContent = 'Nuevo Heading';
+console.log(heading);
+
+
+
+
+// querySelectorAll
+const enlaces = document.querySelectorAll(".navegacion a")
+
+
+
+// getElementID
